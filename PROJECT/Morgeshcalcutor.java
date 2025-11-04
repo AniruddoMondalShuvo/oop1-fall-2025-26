05f9c34281537ba3baf0098ddd34261d835fc315
ANIRUDDO <shuvomondal335@gmail.com>
2025-11-04 16:45:08 +0600

    25-61034-1

---
+++ PROJECT/Morgeshcalcutor.java
@@ -0,0 +1,46 @@
+package PROJECT;
+import java.util.Scanner;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Morgeshcalcutor {
+    public static void main (String [] arg){
+
+       Scanner input = new Scanner(System.in);
+
+       System.out.println("== Bangladesh Mortgage Calculator (BDT) ==");
+
+       System.out.print("Enter loan amount (Principal in BDT): ");
+
+       double principal = input.nextDouble();
+
+       System.out.print("Enter annual interest rate (e.g., 8.5 for 8.5%): ");
+
+       double annualInterestRate = input.nextDouble();
+
+       System.out.print("Enter loan period (in years): ");
+
+       int years = input.nextInt();
+       
+
+
+   }
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+
